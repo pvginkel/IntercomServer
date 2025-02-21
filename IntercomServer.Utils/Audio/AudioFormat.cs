a5dd33ef956d55b2e6dc0ecd167683245244b474
Pieter van Ginkel <pvginkel@gmail.com>
2025-02-21 19:44:31 +0100

    Minor fix.

--- IntercomServer.Utils/Audio/AudioFormat.cs
+++ IntercomServer.Utils/Audio/AudioFormat.cs
@@ -2,20 +2,7 @@
 
 public record AudioFormat(AudioChannelLayout ChannelLayout, int SampleRate, int BitRate)
 {
-    public int BytesPerSecond
-    {
-        get
-        {
-            var channelBytes = ChannelLayout switch
-            {
-                AudioChannelLayout.Mono => 1,
-                AudioChannelLayout.Stereo => 2,
-                _ => throw new ArgumentOutOfRangeException()
-            };
-
-            return SampleRate * channelBytes * (BitRate / 8);
-        }
-    }
+    public int BytesPerSecond => SampleRate * ChannelCount * (BitRate / 8);
 
     public int ChannelCount =>
         ChannelLayout switch
